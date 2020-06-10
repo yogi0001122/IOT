bc7fce1704daef0c031e1681760db613ef743731
yogi0001122 <56965623+yogi0001122@users.noreply.github.com>
2020-06-10 10:18:18 +0530

    Update device_temperature.py

--- azure-iothub/device_temperature.py
+++ azure-iothub/device_temperature.py
@@ -5,25 +5,24 @@ import random
 import time
 import sys
 import wmi
+import random
+import time
+import threading
+import webbrowser
+
 
 # Using the Python Device SDK for IoT Hub:
 #   https://github.com/Azure/azure-iot-sdk-python
 # The sample connects to a device-specific MQTT endpoint on your IoT Hub.
 #import iothub_client
-from azure.iot.device import IoTHubDeviceClient, Message
-# pylint: disable=E0611
-#from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
-#from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
-
+from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 # The device connection string to authenticate the device with your IoT hub.
 # Using the Azure CLI:
 # az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
-CONNECTION_STRING = "HostName=iothub-poc-demo.azure-devices.net;DeviceId=iot-device1;SharedAccessKey=fL8W+ivYhA/tDfs7KLAFk92bvLNO87XvAWRNRIaDJro="
+CONNECTION_STRING = ""
 
-# Using the MQTT protocol.
-#PROTOCOL = IoTHubTransportProvider.MQTT
 MESSAGE_TIMEOUT = 10000
-
+INTERVAL = 5
 # Define the JSON message to send to IoT Hub.
 TEMPERATURE = 20.0
 HUMIDITY = 60
@@ -40,11 +39,53 @@ def iothub_client_init():
     client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
     return client
 
+def device_method_listener(device_client):
+    global INTERVAL
+    while True:
+        method_request = device_client.receive_method_request()
+        print (
+            "\nMethod callback called with:\nmethodName = {method_name}\npayload = {payload}".format(
+                method_name=method_request.name,
+                payload=method_request.payload
+            )
+        )
+        if method_request.name == "SetTelemetryInterval":
+            try:
+                INTERVAL = int(method_request.payload)
+                print (INTERVAL)
+            except ValueError:
+                response_payload = {"Response": "Invalid parameter"}
+                response_status = 400
+            else:
+                response_payload = {"Response": "Executed direct method {}".format(method_request.name)}
+                response_status = 200
+
+        elif method_request.name == "IOT Play":
+            try:
+                print ("Playing Azure Video on Youtube.......")
+                webbrowser.open("https://www.youtube.com/watch?v=smuZaZZXKsU")
+            except:
+                response_payload = {"Response": "Invalid request"}
+                response_status = 400
+            else:
+                response_payload = {"Response": "Executed direct method {}".format(method_request.name)}
+                response_status = 200
+
+        else:
+            response_payload = {"Response": "Direct method {} not defined".format(method_request.name)}
+            response_status = 404
+
+        method_response = MethodResponse(method_request.request_id, response_status, payload=response_payload)
+        device_client.send_method_response(method_response)
+
 def iothub_client_telemetry_sample_run():
 
     try:
         client = iothub_client_init()
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+        device_method_thread = threading.Thread(target=device_method_listener, args=(client,))
+        device_method_thread.daemon = True
+        device_method_thread.start()
 
         while True:
             # Build the message with simulated telemetry values.
@@ -59,15 +100,20 @@ def iothub_client_telemetry_sample_run():
                   msg_txt_formatted = MSG_TXT % (temperature, sensorname)
                   print (msg_txt_formatted)
                   message = Message(msg_txt_formatted)
-                  if temperature > 30:
-                      message.custom_properties["temperatureAlert"] = "true"
-                  else:
-                      message.custom_properties["temperatureAlert"] = "false"
-                  # Send the message.
-                  print( "Sending message: {}".format(message) )
-                  client.send_message(message)
-                  print ( "Message successfully sent" )
-            time.sleep(5)
+
+            # Add a custom application property to the message.
+            # An IoT hub can filter on these properties without access to the message body.
+            if temperature > 30:
+              message.custom_properties["temperatureAlert"] = "true"
+            else:
+              message.custom_properties["temperatureAlert"] = "false"
+
+            # Send the message.
+            print( "Sending message: {}".format(message) )
+            client.send_message(message)
+            print ( "Message successfully sent" )
+            print ( "IoTHubClient waiting for commands, press Ctrl-C to exit" )
+            time.sleep(INTERVAL)
 
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
